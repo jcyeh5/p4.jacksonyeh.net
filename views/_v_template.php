852f037971d3c680b038b1d43d4b6a959ac027c4
Jackson Yeh <jcyeh5@hotmail.com>
2013-12-18 07:44:12 -0500

    updated links

--- views/_v_template.php
+++ views/_v_template.php
@@ -22,8 +22,8 @@
 
 					<!-- Menu for users who are logged in -->
 					<?php if($user): ?>
-						<li id="mainnav_postopinions"><a href='/posts/add'>Review Restaurant</a></li>
-						<li id="mainnav_postopinions"><a href='/posts/add'>Add Restaurant</a></li>						
+						<li id="mainnav_postopinions"><a href='/restaurants/index'>Review Restaurant</a></li>
+						<li id="mainnav_postopinions"><a href='/restaurants/add'>Add Restaurant</a></li>						
 						<li id="mainnav_members"><a href='/posts/users'>Find Friends</a></li>
 						<li id="mainnav_editprofile"><a href='/users/profile'>About Me</a></li>
 						<li id="mainnav_logout"><a href='/users/logout'>Logout</a></li>
